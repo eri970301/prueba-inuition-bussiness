292a5d997d84825538cbc4697297bda37ff14f5d
Erika Otalvaro <erika970301@live.com>
2021-05-26 15:45:22 -0500

    functions commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <div id="screen" className="screen mostrar">
             <div id="alerta" className="text-center position-absolute start-50 translate-middle-x card_button opacity-0">
               <div className="card-body rounded-lg" style={{ backgroundColor: "#b71ddd" }}>
-                <p className="card-text fw-bold text-white">Haz click en el animal que produjo el sonido</p>
+                <p className="card-text fw-bold text-white" id="aviso">Haz click en el animal que produjo el sonido</p>
               </div>
             </div>
           </div>
@@ -30,28 +30,28 @@ function App() {
             </div>
           </div>
           <div>
-            <img src={burro} className="img_animals burro"></img>
+            <img src={burro} className="img_animals burro" id="burro"></img>
           </div>
           <div>
-            <img src={caballo} className="img_animals caballo"></img>
+            <img src={caballo} className="img_animals caballo" id="caballo"></img>
           </div>
           <div>
-            <img src={perro} className="img_animals perro"></img>
+            <img src={perro} className="img_animals perro" id="perro"></img>
           </div>
           <div>
-            <img src={vaca} className="img_animals vaca"></img>
+            <img src={vaca} className="img_animals vaca" id="vaca"></img>
           </div>
           <div>
-            <img src={conejo} className="img_animals conejo"></img>
+            <img src={conejo} className="img_animals conejo" id="conejo"></img>
           </div>
           <div>
-            <img src={marrano} className="img_animals marrano"></img>
+            <img src={marrano} className="img_animals marrano" id="marrano"></img>
           </div>
           <div>
-            <img src={gallina} className="img_animals gallina"></img>
+            <img src={gallina} className="img_animals gallina" id="gallina"></img>
           </div>
           <div>
-            <img src={oveja} className="img_animals oveja" ></img>
+            <img src={oveja} className="img_animals oveja" id="oveja"></img>
           </div>
     </div>
   );
--- src/funciones.js
+++ src/funciones.js
@@ -1,8 +1,19 @@
 var totalTime = 3;
 
+function burro(){
+   document.innerHTML = '<audio src="../src/sounds/burro.mp3" autoPlay></audio>';
+    document.querySelector('#caballo').classList.add('mostrar')
+    document.querySelector('#perro').classList.add('mostrar')
+    document.querySelector('#marrano').classList.add('mostrar')
+    document.querySelector('#gallina').classList.add('mostrar')
+    document.querySelector('#burro').addEventListener('click', function burroSelected(){
+        document.querySelector('#aviso').innerHTML = 'Es correcto!'
+    })
+}
+
 function game(){
     document.querySelector('#count').classList.remove('mostrar');
-    document.querySelector('#count').innerHTML = totalTime + '<audio src="../src/sounds/pito.mp3" autoPlay></audio>';
+    document.querySelector('#count').innerHTML = totalTime + '<audio> <source src="pito.mp3" type="audio/mpeg" /></audio>';
     var counter = setTimeout(game, 1000);
     totalTime = totalTime - 1;
     if (totalTime == (-1)){
@@ -10,6 +21,7 @@ function game(){
         document.querySelector('#count').classList.add('mostrar');
         document.querySelector('#alerta1').classList.add('mostrar')
         document.querySelector('#screen').classList.remove('mostrar');
+        burro()
     }
 }
 
